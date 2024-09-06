af56b00fb1adaacdc93777ee2b521df113ffc4a2
rawatcode <rawatcode@gmail.com>
2024-09-06 17:20:58 +0530

    day 23 done

---
+++ Day_23.cpp
@@ -0,0 +1,80 @@
+/*
+https://www.hackerrank.com/challenges/30-binary-trees/problem?isFullScreen=true
+*/
+
+
+#include <iostream>
+#include <cstddef>
+#include <queue>
+#include <string>
+#include <cstdlib>
+
+using namespace std;	
+class Node{
+    public:
+        int data;
+        Node *left,*right;
+        Node(int d){
+            data=d;
+            left=right=NULL;
+        }
+};
+class Solution{
+    public:
+  		Node* insert(Node* root, int data){
+            if(root==NULL){
+                return new Node(data);
+            }
+            else{
+                Node* cur;
+                if(data<=root->data){
+                    cur=insert(root->left,data);
+                    root->left=cur;
+                }
+                else{
+                   cur=insert(root->right,data);
+                   root->right=cur;
+                 }           
+           return root;
+           }
+        }
+
+	void levelOrder(Node * root){
+      //Write your code here
+      
+      if(root == NULL){
+          return;
+      }
+      
+      queue<Node*> q;
+      q.push(root);
+      
+      
+      while(!q.empty()){
+          Node* currentNode = q.front();
+          cout << currentNode->data << " ";
+          q.pop();
+          
+          if(currentNode->left != NULL){
+              q.push(currentNode->left);
+          }
+          
+          if(currentNode->right != NULL){
+              q.push(currentNode->right);
+          }
+      }  
+	}
+
+};//End of Solution
+int main(){
+    Solution myTree;
+    Node* root=NULL;
+    int T,data;
+    cin>>T;
+    while(T-->0){
+        cin>>data;
+        root= myTree.insert(root,data);
+    }
+    myTree.levelOrder(root);
+    return 0;
+}
